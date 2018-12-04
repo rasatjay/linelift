696b9df992ea46269fd856e2ffd13a4b9c922672
NB-SSK <rasatjay@ssk.co.th>
2018-12-04 15:24:42 +0700

    new2

--- liff-starter.js
+++ liff-starter.js
@@ -28,7 +28,7 @@ function initializeApp(data) {
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
         liff.sendMessages([{
             type: 'text',
-            text: "You ve successfully sent a message! Hooray!"
+            text: "You"
         }, {
             type: 'sticker',
             packageId: '2',
