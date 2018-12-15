d50a046691764509dfc397a8d3a91ab6d3d6aea3
NB-SSK <rasatjay@ssk.co.th>
2018-12-15 11:06:24 +0700

    backtooriginal

--- liff-starter.js
+++ liff-starter.js
@@ -26,7 +26,7 @@ function initializeApp(data) {
 
     // sendMessages call
     document.getElementById('sendmessagebutton').addEventListener('click', function () {
-		var testtext = document.getElementById('regioncheck').value;
+		var testtext = "select".append(document.getElementById('regioncheck').value).append(document.getElementById('provincecheck').value);
         liff.sendMessages([{
             type: 'text',
             text: testtext
