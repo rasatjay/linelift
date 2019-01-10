651c521b72805ef8a97b0fc34810105f881db97a
NB-SSK <rasatjay@ssk.co.th>
2019-01-10 15:07:41 +0700

    almost finished

--- liff-starter.js
+++ liff-starter.js
@@ -62,9 +62,245 @@ function initializeApp(data) {
 		}
 		if (distanceboxnotempty && regionboxnotempty && monthboxnotempty) {
 			liff.sendMessages([{
-				type: 'text',
-				text: sendtext
-			}
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "header": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "none",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "horizontal",
+          "flex": 10,
+          "contents": [
+            {
+              "type": "box",
+              "layout": "vertical",
+              "flex": 5,
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "Intania Chula Mini Marathon 2019",
+                  "size": "md",
+                  "align": "start",
+                  "weight": "bold",
+                  "color": "#6A6A6A",
+                  "action": {
+                    "type": "postback",
+                    "data": "action=reqraceinfo&raceid="
+                  },
+                  "wrap": true
+                },
+                {
+                  "type": "text",
+                  "text": "Faculty of Engineering, Chulalongkorn University Bangkok,  10330",
+                  "margin": "none",
+                  "size": "xs",
+                  "color": "#B9B9B9",
+                  "action": {
+                    "type": "postback",
+                    "data": "action=reqraceinfo&raceid="
+                  },
+                  "wrap": true
+                },
+                {
+                  "type": "spacer"
+                }
+              ]
+            },
+            {
+              "type": "separator",
+              "margin": "sm"
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "flex": 2,
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "18",
+                  "size": "lg",
+                  "align": "center",
+                  "gravity": "top",
+                  "weight": "bold",
+                  "color": "#E80404"
+                },
+                {
+                  "type": "text",
+                  "text": "Nov",
+                  "align": "center",
+                  "gravity": "top"
+                },
+                {
+                  "type": "text",
+                  "text": "2019",
+                  "margin": "xs",
+                  "size": "xs",
+                  "align": "center",
+                  "gravity": "center",
+                  "color": "#CACACA"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "body": {
+      "type": "box",
+      "layout": "vertical",
+      "spacing": "none",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "md",
+          "margin": "md",
+          "contents": [
+            {
+              "type": "text",
+              "text": "เปิดรับสมัคร  27 ตุลาคม 2561",
+              "margin": "xs",
+              "size": "xs",
+              "wrap": true
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "spacing": "xs",
+              "contents": [
+                {
+                  "type": "spacer"
+                },
+                {
+                  "type": "text",
+                  "text": "5 km",
+                  "flex": 2,
+                  "size": "sm",
+                  "align": "start",
+                  "color": "#6A6A6A"
+                },
+                {
+                  "type": "text",
+                  "text": "(6:00 am)",
+                  "flex": 3,
+                  "margin": "sm",
+                  "size": "xs",
+                  "align": "start",
+                  "gravity": "center",
+                  "color": "#8C8C8C"
+                },
+                {
+                  "type": "text",
+                  "text": "550 THB",
+                  "flex": 4,
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#8C8C8C"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "margin": "xs",
+              "contents": [
+                {
+                  "type": "spacer"
+                },
+                {
+                  "type": "text",
+                  "text": "100 km",
+                  "flex": 2,
+                  "size": "sm",
+                  "align": "start",
+                  "color": "#6A6A6A"
+                },
+                {
+                  "type": "text",
+                  "text": "(6:00 am)",
+                  "flex": 3,
+                  "margin": "sm",
+                  "size": "xs",
+                  "align": "start",
+                  "gravity": "center",
+                  "color": "#989898"
+                },
+                {
+                  "type": "text",
+                  "text": "700 THB",
+                  "flex": 4,
+                  "size": "sm",
+                  "align": "end",
+                  "color": "#8C8C8C"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "footer": {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "box",
+          "layout": "vertical",
+          "contents": [
+            {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Register",
+                    "uri": "https://linecorp.com"
+                  },
+                  "color": "#CE0707",
+                  "style": "link"
+                },
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Visit site",
+                    "uri": "https://linecorp.com"
+                  },
+                  "color": "#CE0707",
+                  "style": "link"
+                }
+              ]
+            },
+            {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "uri",
+                    "label": "Save race",
+                    "uri": "https://linecorp.com"
+                  },
+                  "color": "#E33D24",
+                  "style": "primary"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
 		
 			//, {
 			//    type: 'sticker',
